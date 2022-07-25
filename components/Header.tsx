17bbb39da3e4d2c97e3902643ff527a7a6255ed0
Jacob Ross <jacob.ross98@outlook.com>
2022-07-24 21:56:04 -0400

    added button to add to server

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,11 @@ const Header = ({ img }: HeaderProps) => {
         <Image alt='Profile Picture' src={img} width={50} height={50} className="rounded-full"></Image>
       </div>
       <div className='ml-auto pr-2'>
+        <a href='https://discord.com/api/oauth2/authorize?client_id=262709306565066753&permissions=8&scope=bot' target='_blank' rel='noreferrer'>
+          <button className='bg-green-500 hover:bg-green-700 text-white transistion duration-300 p-2 rounded-lg'>Add to server</button>
+        </a>
+      </div>
+      <div className='pr-2'>
         <button className='bg-blue-500 hover:bg-blue-700 text-white transistion duration-300 p-2 rounded-lg' onClick={() => signOut()}>Sign Out</button>
       </div>
     </div>
